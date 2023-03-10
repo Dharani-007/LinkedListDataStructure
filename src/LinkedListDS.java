1273215b739cd79018dba05517f27c3b102e01c8
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-10 16:58:52 +0530

    Added : Ability to delete the first element in the LinkedList of sequence 56->30->70

--- src/LinkedListDS.java
+++ src/LinkedListDS.java
@@ -1,8 +1,8 @@
+import java.util.Scanner;
+
 class Node {
     public int data;
     public Node next;
-
-    //Node Constructor for Creating New Node
     Node(int data) {
         this.data = data;
         this.next = null;
@@ -10,37 +10,72 @@ class Node {
 }
 
 class LinkedList {
+
+    int location = 0;
+
     Node head;
     Node tail;
-
     //Insert Data in New Node
     public void insertFirst(int data) {
         Node newNode = new Node(data);
         //Adding Data in Node
-        if (head == null) {
+        if(head == null) {
             head = newNode;
             tail = newNode;
         } else {
             newNode.next = head;
             head = newNode;
         }
+        location++;
+        System.out.println("Location:"+location);
     }
 
     //Insert Data from Last
     public void insertLast(int data) {
         Node newNode = new Node(data);
-        if (tail == null) {
+        if(tail == null) {
             head = newNode;
             tail = newNode;
         } else {
             tail.next = newNode;
             tail = newNode;
         }
+        location++;
+        System.out.println("Location:"+location);
     }
+
+    //Insert New Node after Given Node
+    public void insertNthPosition(int data,int nthdata) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            Node nextNode;
+            while (temp != null) {
+                nextNode = temp.next;
+                if(temp.data == nthdata) {
+                    temp.next = newNode;
+                    newNode.next = nextNode;
+                }
+//                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+    }
+    public void pop() {
+        if(head == null) {
+            System.out.println("Linked List is Empty.");
+        } else {
+            head = head.next;
+        }
+    }
+
 
     //Display Node in Linked List
     public void showLinkedList() {
-        if (head == null) {
+        if(head == null) {
             System.out.println("Linked List is Empty.");
         } else {
             Node temp = head;
@@ -54,16 +89,53 @@ class LinkedList {
 
 public class LinkedListDS {
     public static void main(String[] args) {
-        //Welcome Message
+
         System.out.println("Welcome to LinkedList Data Structures Problems.");
+
+        Scanner sc = new Scanner(System.in);
 
         LinkedList linkedList = new LinkedList();
 
-        //Insert 70, 50, 36 in Linked List Nodes
-        linkedList.insertLast(56);
-        linkedList.insertLast(30);
-        linkedList.insertLast(70);
+        int choice;
+        do {
+            System.out.println();
+            System.out.println("SIMPLE LINKED LIST OPERATIONS");
+            System.out.println("1. INSERT FIRST");
+            System.out.println("2. INSERT LAST ");
+            System.out.println("3. DISPLAY LIST ");
+            System.out.println("4. INSERT AT Nth POSITION ");
+            System.out.println("5. DELETE FIRST POSITION ");
+            System.out.println("6. EXIT ");
+            System.out.println("Enter the Choice for Operation : ");
+            choice = sc.nextInt();
 
-        linkedList.showLinkedList();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the Data to Insert at First : ");
+                    int dataFirst = sc.nextInt();
+                    linkedList.insertFirst(dataFirst);
+                    break;
+                case 2:
+                    System.out.println("Enter the Data to Insert at Last : ");
+                    int dataLast = sc.nextInt();
+                    linkedList.insertLast(dataLast);
+                    break;
+                case 3:
+                    linkedList.showLinkedList();
+                    break;
+                case 4:
+                    System.out.println("Enter the Data After which to Insert New Data : ");
+                    int nthData = sc.nextInt();
+                    System.out.println("Enter the Data : ");
+                    int data = sc.nextInt();
+                    linkedList.insertNthPosition(data,nthData);
+                    break;
+                case 5:
+                    linkedList.pop();
+                    System.out.println("After First Node Delete");
+                    linkedList.showLinkedList();
+                    break;
+            }
+        }while(choice != 6);
     }
 }
