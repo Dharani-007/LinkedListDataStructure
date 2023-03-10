b0e1d37eec1562226d33dd2bfc6e191820085eed
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-10 16:52:58 +0530

    Added :Ability to insert 30 between 56 and 70

--- src/LinkedListDS.java
+++ src/LinkedListDS.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Node {
     public int data;
     public Node next;
@@ -10,24 +12,63 @@ class Node {
 }
 
 class LinkedList {
+    int location = 0;
     Node head;
     Node tail;
+
     //Insert Data in New Node
     public void insertFirst(int data) {
         Node newNode = new Node(data);
         //Adding Data in Node
-        if(head == null) {
+        if (head == null) {
             head = newNode;
             tail = newNode;
         } else {
             newNode.next = head;
             head = newNode;
         }
+        location++;
+        System.out.println("Location:" + location);
+    }
+
+    //Insert Data from Last
+    public void insertLast(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        location++;
+        System.out.println("Location:" + location);
+    }
+
+    //Insert New Node after Given Node
+    public void insertNthPosition(int data, int nthdata) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            Node nextNode;
+            while (temp != null) {
+                nextNode = temp.next;
+                if (temp.data == nthdata) {
+                    temp.next = newNode;
+                    newNode.next = nextNode;
+                }
+//                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
     }
 
     //Display Node in Linked List
     public void showLinkedList() {
-        if(head == null) {
+        if (head == null) {
             System.out.println("Linked List is Empty.");
         } else {
             Node temp = head;
@@ -41,15 +82,42 @@ class LinkedList {
 
 public class LinkedListDS {
     public static void main(String[] args) {
+        //Welcome Message
+        System.out.println("Welcome to LinkedList Data Structures Problems.");
 
-        System.out.println("Welcome to LinkedList Data Structures Problem.");
+        Scanner sc = new Scanner(System.in);
 
         LinkedList linkedList = new LinkedList();
 
-        linkedList.insertFirst(70);
-        linkedList.insertFirst(30);
-        linkedList.insertFirst(56);
+        int choice;
+        do {
+            System.out.println("SIMPLE LINKED LIST OPERATIONS");
+            System.out.println("1. INSERT FIRST \n2. INSERT LAST \n3. DISPLAY LIST \n4. INSERT AT Nth POSITION \n5. EXIT \nEnter the Choice for Operation : ");
+            choice = sc.nextInt();
 
-        linkedList.showLinkedList();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the Data to Insert at First : ");
+                    int dataFirst = sc.nextInt();
+                    linkedList.insertFirst(dataFirst);
+                    break;
+                case 2:
+                    System.out.println("Enter the Data to Insert at Last : ");
+                    int dataLast = sc.nextInt();
+                    linkedList.insertLast(dataLast);
+                    break;
+                case 3:
+                    linkedList.showLinkedList();
+                    break;
+                case 4:
+                    System.out.println("Enter the Data After which to Insert New Data : ");
+                    int nthData = sc.nextInt();
+                    System.out.println("Enter the Data : ");
+                    int data = sc.nextInt();
+                    linkedList.insertNthPosition(data, nthData);
+                    break;
+            }
+        }
+        while (choice != 5);
     }
 }
